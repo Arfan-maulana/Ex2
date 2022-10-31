f21c348a8754139379640e193a973fc52fe05262
Muhammad Arfan <muhammad.arfan.ft21@mail.umy.ac.id>
2022-10-31 16:44:39 +0700

    Membuat Binary Sort

--- Ex2/Program.cs
+++ Ex2/Program.cs
@@ -9,7 +9,8 @@ namespace Ex2
      class Program
     {
         //deklarasi array int dengan ukuran 38
-        private int[] fann = new int[38];
+        public int[] fann = new int[38];
+
 
         //Deklarasi variable int untuk menyimpan banyaknya data pada array
         private int n;
@@ -70,6 +71,75 @@ namespace Ex2
                     }
                 }
             }
+        }
+        public void input()
+        {
+            int n;
+            // get the number of element to store in the array
+            int i;
+
+            while (true)
+            {
+                Console.Write("Enter the number elements in the array  ");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if ((n > 0) && (n <= 20))
+                    break;
+                else
+                    Console.WriteLine("\nArray should have minimum 1 and maximum 20 elemets.\n");
+            }
+            // accpect array element
+            Console.WriteLine("");
+            Console.WriteLine("-----------------------------");
+            Console.WriteLine(" Enter array elements ");
+            Console.WriteLine("-----------------------------");
+            for (i = 0; i < n; i++)
+            {
+                Console.Write("<" + (i + 1) + ">");
+                string s1 = Console.ReadLine();
+                fann[i] = Int32.Parse(s1);
+            }
+
+        }
+        public void BinarySearch()
+        {
+            char ch;
+            do
+            {
+
+                //accpect the number to be searched 
+                Console.Write("\nEnter element want you to Search : ");
+                int item = Convert.ToInt32(Console.ReadLine());
+
+                //applt binary search
+                int lowerbound = 0;
+                int upperbound = n - 1;
+
+                //obtain the index of the middle elemets
+                int mid = (lowerbound + upperbound) / 2;
+                int ctr = 1;
+
+                //loop to search for the elements in the array 
+                while ((item != fann[mid]) && (lowerbound <= upperbound))
+                {
+                    if (item > fann[mid])
+                        lowerbound = mid + 1;
+                    else
+                        upperbound = mid - 1;
+                    mid = (lowerbound + upperbound) / 2;
+                    ctr++;
+                }
+                if (item == fann[mid])
+                    Console.WriteLine("\n" + item.ToString() + "Found at position" + (mid + 1).ToString());
+                else
+                    Console.WriteLine("\n" + item.ToString() + "Not Found in the array \n");
+                Console.WriteLine("\nNumber of comprasion : " + ctr);
+
+                Console.Write("\nContinue search (y/n) : ");
+                ch = char.Parse(Console.ReadLine());
+
+            } while ((ch != 'y') || (ch == 'y'));
+
         }
 
         static void Main(string[] args)
