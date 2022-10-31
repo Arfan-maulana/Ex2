57b32734c960303eea428c69d7840f347302da8f
Muhammad Arfan <muhammad.arfan.ft21@mail.umy.ac.id>
2022-10-31 16:33:44 +0700

    membuat publick void read untuk bubble short

--- Ex2/Program.cs
+++ Ex2/Program.cs
@@ -13,6 +13,32 @@ namespace Ex2
 
         //Deklarasi variable int untuk menyimpan banyaknya data pada array
         private int n;
+        public void read()
+        {
+            //Menerima angka untuk menentukan banyaknya data yang disimpan pada array
+            while (true)
+            {
+                Console.WriteLine("Masukkan Banyak Elemen pada Array");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if (n <= 38)
+                    break;
+                else
+                    Console.WriteLine("\nArray dapat mempunyai maksimal 38 elemen.\n");
+            }
+            Console.WriteLine("");
+            Console.WriteLine("----------------------------------------------------------");
+            Console.WriteLine("Masukkan elemen Array");
+            Console.WriteLine("----------------------------------------------------------");
+
+            //Pengguna Memasukkan elemen pada array
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("<" + (i + 1) + ">");
+                string s = Console.ReadLine();
+                fann[i] = Int32.Parse(s);
+            }
+        }
 
         static void Main(string[] args)
         {
